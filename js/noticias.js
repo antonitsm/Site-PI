c717ab963ba8a2e53e4190a565d34c3a701437e6
G4BVEI <gabdaveiga@gmail.com>
2024-10-19 18:29:54 -0300

    a

---
+++ js/noticias.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
