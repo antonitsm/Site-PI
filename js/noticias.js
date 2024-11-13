83ad8b762c8b9e15c98399eddf2973ada732c72c
antonitsm <antonitsm30@gmail.com>
2024-11-13 20:15:56 -0300

    feito carrossel da seção de noticias

--- js/noticias.js
+++ js/noticias.js
@@ -1,17 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper', {
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.carousel img');
+const totalImages = images.length;
+const autoAdvanceTime = 3000;
+
+function showImage(index) {
+    images.forEach((img, i) => {
+      img.classList.toggle('active', i === index);
     });
-});
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % totalImages;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+    showImage(currentImageIndex);
+}
+
+setInterval(nextImage, autoAdvanceTime);
