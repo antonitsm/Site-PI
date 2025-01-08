b5c19d2eab410dda352e618e0eddeef2cb355496
antonitsm <antonitsm30@gmail.com>
2025-01-07 22:09:24 -0300

    responsivo até demais

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 function showCard(cardId) {
-    // Get the selected card
     var selectedCard = document.getElementById(cardId);
     
-    // Check if the selected card is currently visible
     if (selectedCard.style.display === 'block') {
-        // If it's visible, hide it
         selectedCard.style.display = 'none';
     } else {
-        // If it's not visible, hide all other cards first
         var cards = document.querySelectorAll('.info-card');
         cards.forEach(function(card) {
             card.style.display = 'none';
         });
         
-        // Then show the selected card
         selectedCard.style.display = 'block';
+    }
+}
+function toggleResponsiveMenu() {
+    const menu = document.querySelector('.responsive-menu');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
     }
 }
